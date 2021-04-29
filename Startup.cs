ff9ea0b6273893294537cb433ea49830ef4df89c
Livia Benedicto <livia.benedicto@hotmail.com>
2021-04-29 13:15:14 -0300

    Adding authentication packages and configuration

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Monsterflix.Api.Configurations;
 using Monsterflix.Api.Data;
@@ -11,6 +16,7 @@ using Monsterflix.Api.Repositories;
 using Monsterflix.Api.Repositories.Contracts;
 using Monsterflix.Api.Services;
 using Monsterflix.Api.Services.Contracts;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Monsterflix.Api
 {
@@ -28,13 +34,34 @@ namespace Monsterflix.Api
         {
             // Adicionando configurações do appsettings
             AppSettingsProvider.Settings = Configuration;
-            
+
             services.AddControllers();
 
             #region Serviços adicionados
             services.AddMvc();
 
             services.AddResponseCompression();
+
+            // Autenticação Token Jwt Bearer
+            byte[] key = Encoding.ASCII.GetBytes(AppSettingsProvider.Settings["KeyTokenJwtBearer"]);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             // Banco de dados - Docker
             services.AddDbContext<DataContext>(options => options.UseSqlServer(AppSettingsProvider.Settings["ConnectionStrings:connectionString"]));
@@ -45,13 +72,37 @@ namespace Monsterflix.Api
             services.AddScoped<IProfileMovieRepository, ProfileMovieRepository>();
             services.AddScoped<IMovieRepository, MovieRepository>();
             services.AddScoped<ITheMovieDBService, TheMovieDBService>();
+            services.AddScoped<ITokenJwtService, TokenJwtService>();
 
+            // Swagger
             services.AddSwaggerGen(options =>
-			{
-				options.SwaggerDoc("v1", new OpenApiInfo { Title = "Monsterflix Api", Version = "v1" });
-			});
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Monsterflix Api",
+                    Version = "v1",
+                    Description = "É tipo um Netflix, só que com energia alternativa e ASP.NET!",
+                    Contact = new OpenApiContact { Name = "LivBenedicto", Url = new Uri("https://github.com/LivBenedicto/Monsterflix") }
+                });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" } },
+						new[] { "", "" }
+					}
+				});
+            });
 
-			services.AddSwaggerGenNewtonsoftSupport();    
+            services.AddSwaggerGenNewtonsoftSupport();
             #endregion
         }
 
@@ -67,20 +118,22 @@ namespace Monsterflix.Api
 
             // Adicionando documentação - Swagger
             app.UseSwagger();
-			app.UseSwaggerUI(options =>
-			{
-				options.SwaggerEndpoint("/swagger/v1/swagger.json", "Monsterflix API");
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Monsterflix API");
 
-				options.RoutePrefix = "swagger";
+                options.RoutePrefix = "swagger";
 
-				options.DocumentTitle = "Monsterflix API";
+                options.DocumentTitle = "Monsterflix API";
 
-				options.DisplayRequestDuration();
-				options.EnableFilter();
-			});
+                options.DisplayRequestDuration();
+                options.EnableFilter();
+            });
 
             app.UseRouting();
 
+            // Adicionando autenticação
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
